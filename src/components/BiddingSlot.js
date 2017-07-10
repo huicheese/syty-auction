ec1e03a123be59c8bbf34d487d92d402f055b2aa
Zhang Xiawei <zhang_xiawei@hotmail.com>
2017-07-11 02:35:09 +0800

    update slots styling

--- src/components/BiddingSlot.js
+++ src/components/BiddingSlot.js
@@ -3,7 +3,10 @@ import React from 'react'
 const BiddingSlot = ({index, bid, bidder}) => {
 	return (
 			<div className="slot-container">
-				<div className="slot-placeholder" >{index}<br/>{bidder}<br/>{bid}</div>
+				<div className="slot-placeholder" >
+					<div className="slot-index">{index}</div>
+					<div className="slot-bid">{bidder}:<br/>$ {bid}</div>
+				</div>
 			</div>
 		)
 }
