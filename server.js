eb04f87fa3917d544962e84c4aa3bf7475e1acaa
HarryTran <ttphu.harry@gmail.com>
2017-07-15 19:25:15 +0800

    Stub dashboard.js

--- server.js
+++ server.js
@@ -26,38 +26,17 @@ app.use(webpackDevMiddleware(compiler, {
 }));
 app.use(require("webpack-hot-middleware")(compiler));
 
-const path = require('path');
-
 require('./server/properties.js').setApp(app);
 require('./server/dashboard.js').setApp(app);
 require('./server/login.js').setApp(app);
 
 require('./server/database.js').initialize();
 
-app.ws('/updates', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log(msg);
-  });
-  console.log('new connection established on /updates');
-});
-
-
-app.post('/submit', function (request, response) {
-    console.log(request.body)
-    console.log(request.cookies)
-    wsInstance.getWss().clients.forEach(client => {
-    //if (client.readyState === WebSocket.OPEN) {
-        client.send("ehhhhes");
-        console.log('222');
-    // }
-    })
-    console.log('done')
-    response.end();
-})
-
+const path = require('path');
 app.get('*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'www', 'index.html'))
-})
+});
+
 const server = app.listen(app.locals.port, function() {
     const host = server.address().address;
     const port = server.address().port;
--- server/dashboard.js
+++ server/dashboard.js
@@ -1,9 +1,31 @@
 exports.setApp = app => {
-    app.ws('/snapshot', (ws, request) => {
-        ws.on('message', msg => {
-        	console.log('Server received:', msg);
-        });
+    app.ws('/updates', (ws, request) => {
+        ws.send(JSON.stringify({slots: stubSlots, events: stubEvents}));
+    });
 
-        //send snapshot
+    app.post('/submit', (request, response) => {
+
     });
 };
+
+const getRandomArbitrary = (min, max) => Math.round((Math.random() * (max - min) + min) * 100) / 100
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+const nameArray = ["Darwin", "Paris", "Jackie", "Dominick", "Abel", "Nelson", "Jeff", "Ivan", "Gene", "Bill", "William", "Myron", "Clayton", "Bryant", "Johnie", "Graig", "Elliott", "Dante", "Benjamin", "Brant", "Bertram", "Morgan", "Johnny", "Jonathan", "Wilfred", "Robert", "Robin", "Mohammed", "Joey", "Bradly", "Denver", "Elden", "Ryan", "Leigh", "Jc", "Asa", "Hayden", "Darrell", "Von", "Gary", "Augustus", "Alphonso", "Logan", "Leon", "Marquis", "Miguel", "Ignacio", "Don", "Derrick", "Jarod"]
+const getRandomName = () => nameArray[getRandomInt(0,nameArray.length)]
+
+const stubSlots = new Array(15).fill().map(
+  (e,i) => ({
+    index: i+1,
+    highestBid: getRandomArbitrary(1, 1000),
+    name: getRandomName()
+  })
+)
+
+const stubEvents = new Array(29).fill().map(
+  (e,i) => ({
+    "bidder": getRandomName(),
+    "bid": getRandomArbitrary(1, 100),
+    "slot": getRandomInt(1,25)
+  })
+)
