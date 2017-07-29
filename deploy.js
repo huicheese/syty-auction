5d5b2452136c2a911fd7b3b7cfe3065fd1528fcc
Harry Tran <ttphu.harry@gmail.com>
2017-07-29 20:58:30 +0800

    Heroku

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     var child_process = require('child_process');
-    child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
+    child_process.exec("webpack -p --config webpack.production.js", function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
         if (error !== null) {
