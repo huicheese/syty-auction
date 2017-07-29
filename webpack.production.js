f9f55101540487b615be65673af2d105af909fbe
Harry Tran <ttphu.harry@gmail.com>
2017-07-29 21:23:42 +0800

    Heroku

--- webpack.production.js
+++ webpack.production.js
@@ -24,6 +24,16 @@ module.exports = {
         path: path.join(__dirname, 'www'),
         filename: 'bundle.js',
     },
+    plugins: {
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin()
+    }
     resolve: {
         modules: [
             path.join(__dirname, 'node_modules'),
