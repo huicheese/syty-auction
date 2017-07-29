21c54d4b32ef2857a0323b5389bfee5de2ab8cda
Harry Tran <ttphu.harry@gmail.com>
2017-07-29 21:29:42 +0800

    Heroku

--- webpack.production.js
+++ webpack.production.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const compressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     context: path.join(__dirname, 'src'),
@@ -25,14 +26,20 @@ module.exports = {
         filename: 'bundle.js',
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
+        new webpack.DefinePlugin({ 
+          'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+          }
         }),
-        new webpack.optimize.DedupePlugin(),
         new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.AggressiveMergingPlugin()
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new CompressionPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
     ],
     resolve: {
         modules: [
