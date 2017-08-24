44bb490817a47d44eed509d6c9eb520f09283556
Zhang Xiawei <zhang_xiawei@hotmail.com>
2017-08-25 02:39:49 +0800

    styling bidders in the slot

--- src/components/BiddingSlot.js
+++ src/components/BiddingSlot.js
@@ -3,6 +3,8 @@ import AnimateOnChange from 'react-animate-on-change'
 
 const BiddingSlot = ({index, hasChange, bid, bidder, onSlotClick}) => {
   let animateStyle = bid != undefined ? {opacity: 0.8} : {}
+
+  let bidders = [bidder];
 	return (
 
 			<div className="slot-container slot-mask" onClick={onSlotClick}>
@@ -12,8 +14,12 @@ const BiddingSlot = ({index, hasChange, bid, bidder, onSlotClick}) => {
           {bid && 
             <div className="slot-bid"><span>$ </span><span>{Intl.NumberFormat().format(bid || 0)}</span></div>}
 
-          { bidder &&
-            <div className="slot-bid">{bidder}</div> }
+          {bidders &&
+            <div className="slot-bidder">
+              {bidders.length <= 2 && bidders.map((e,i) => <div key={e}>{e}</div>)}
+              {bidders.length >= 3 && <div style={{fontStyle:"italic"}}>{bidders.length} Bidders</div>}
+            </div>
+          }
           <AnimateOnChange 
           baseClassName="slot-highlight"
           animationClassName="slot-highlight-blink"
