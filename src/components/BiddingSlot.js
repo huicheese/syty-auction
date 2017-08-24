16d3b49580027f65a78f9c6e9b92d5e7d5be405f
Zhang Xiawei <zhang_xiawei@hotmail.com>
2017-08-25 01:57:13 +0800

    styling slot index and bid

--- src/components/BiddingSlot.js
+++ src/components/BiddingSlot.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import AnimateOnChange from 'react-animate-on-change'
 
-
 const BiddingSlot = ({index, hasChange, bid, bidder, onSlotClick}) => {
   let animateStyle = bid != undefined ? {opacity: 0.8} : {}
 	return (
 
 			<div className="slot-container slot-mask" onClick={onSlotClick}>
         <div className="slot-placeholder">
+        <div className="slot-corner" />
           <div className="slot-index">{index + 1}</div>
+          {bid && 
+            <div className="slot-bid"><span>$ </span><span>{Intl.NumberFormat().format(bid || 0)}</span></div>}
+
           { bidder &&
-            <div className="slot-bid">{bidder}:<br/>$ {bid}</div> }
+            <div className="slot-bid">{bidder}</div> }
           <AnimateOnChange 
           baseClassName="slot-highlight"
           animationClassName="slot-highlight-blink"
